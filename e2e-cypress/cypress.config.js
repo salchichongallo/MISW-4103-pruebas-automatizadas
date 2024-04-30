e9b5f1acbe44c031f674f679f4dd52de3051252b
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-04-30 08:18:19 -0500

    build(cypress): set base URL

--- e2e-cypress/cypress.config.js
+++ e2e-cypress/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:2368/ghost',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
