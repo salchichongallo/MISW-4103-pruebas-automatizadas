6100d110684b2131a5f5d2c2d450402e020a9de0
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-05-01 19:03:03 -0500

    ci: use config files to exclude onboarding

--- e2e-cypress/cypress.config-onboarding.js
+++ e2e-cypress/cypress.config-onboarding.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:2368/ghost',
-    excludeSpecPattern: 'cypress/e2e/1_onboarding.cy.js',
+    specPattern: 'cypress/e2e/1_onboarding.cy.js',
   },
 });
