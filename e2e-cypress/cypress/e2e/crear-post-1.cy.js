eae7a3b1b6a191efd3c03a29cc72daec4d4140aa
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-05-05 12:42:42 -0500

    refactor: wait for dashboard

--- e2e-cypress/cypress/e2e/crear-post-1.cy.js
+++ e2e-cypress/cypress/e2e/crear-post-1.cy.js
@@ -1,25 +1,28 @@
 import { PostsPage } from './pages/PostPage';
 import { LoginPage } from './pages/LoginPage';
+import { DashboardPage } from './pages/dashboard-page';
 
 describe('Create a post', () => {
-    const loginPage = new LoginPage();
-    const postsPage = new PostsPage();
+  const loginPage = new LoginPage();
+  const postsPage = new PostsPage();
+  const dashboardPage = new DashboardPage();
 
-    beforeEach(() => {
-        loginPage.visit();
-        loginPage.fillEmail(Cypress.env('email'));
-        loginPage.fillPassword(Cypress.env('password'));
-        loginPage.submit();
-    });
+  beforeEach(() => {
+    loginPage.visit();
+    loginPage.fillEmail(Cypress.env('email'));
+    loginPage.fillPassword(Cypress.env('password'));
+    loginPage.submit();
+    dashboardPage.waitFor();
+  });
 
-    it('As a user I log in, enter the Posts section, create a new Post and verify its creation in the Posts list', () => {
-        postsPage.visit();
-        postsPage.newPost();
-        postsPage.fillTitle('test post');
-        postsPage.clickDescriptionPost();
-        postsPage.publish();
-        postsPage.backToEditor();
-        postsPage.visit();
-        postsPage.verifyPost('test post');
-    });
+  it('As a user I log in, enter the Posts section, create a new Post and verify its creation in the Posts list', () => {
+    postsPage.visit();
+    postsPage.newPost();
+    postsPage.fillTitle('test post');
+    postsPage.clickDescriptionPost();
+    postsPage.publish();
+    postsPage.backToEditor();
+    postsPage.visit();
+    postsPage.verifyPost('test post');
+  });
 });
--- e2e-cypress/cypress/e2e/crear-post-4.cy.js
+++ e2e-cypress/cypress/e2e/crear-post-4.cy.js
@@ -1,15 +1,18 @@
 import { PostsPage } from './pages/PostPage';
 import { LoginPage } from './pages/LoginPage';
+import { DashboardPage } from './pages/dashboard-page';
 
 describe('Create a post', () => {
     const loginPage = new LoginPage();
     const postsPage = new PostsPage();
+    const dashboardPage = new DashboardPage();
 
     beforeEach(() => {
         loginPage.visit();
         loginPage.fillEmail(Cypress.env('email'));
         loginPage.fillPassword(Cypress.env('password'));
         loginPage.submit();
+        dashboardPage.waitFor();
     });
 
     it('As a user I log in, enter the Posts section, create a new Post, without publishing it and it should not appear on the published page', () => {
