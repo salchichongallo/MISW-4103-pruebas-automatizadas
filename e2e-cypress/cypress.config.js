2ec13b498a7dd6e00948c266e7fb4f963e490894
Jaime Gallo <j.gallom@uniandes.edu.co>
2024-05-05 22:03:04 -0500

    refactor: update cypress.config.js

--- e2e-cypress/cypress.config.js
+++ e2e-cypress/cypress.config.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:2368',
+    baseUrl: process.env.GHOST_BASE_URL || 'http://localhost:2368',
     excludeSpecPattern: 'cypress/e2e/1_onboarding.cy.js',
   },
 });
