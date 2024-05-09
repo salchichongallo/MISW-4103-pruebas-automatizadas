bafd156e209998991282bf4b86211caa80e97fe4
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-05-08 21:00:56 -0500

    feat(cypress): take screenshots delete tags

--- e2e-cypress/cypress/e2e/delete-tag-4.cy.js
+++ e2e-cypress/cypress/e2e/delete-tag-4.cy.js
@@ -21,30 +21,45 @@ describe('DELTAG4 - Admins cancel deletion of tags', () => {
   `, () => {
     const loginPage = new LoginPage();
     loginPage.visit();
+    cy.step('The login page');
+
     loginPage.fillEmail(Cypress.env('email'));
     loginPage.fillPassword(Cypress.env('password'));
     loginPage.submit();
+    cy.step('I sign-in with "<email>" and "<password>"');
 
     const dashboardPage = new DashboardPage();
     dashboardPage.waitFor();
+    cy.step('I wait for the dashboard');
 
     const tagsPage = new TagsPage();
     tagsPage.visit();
+    cy.step('I navigate to tags page');
+
     const tagName =
       faker.person.firstName() + Math.random().toString(36).slice(2, 10);
     const tagForm = tagsPage.newTag();
     tagForm.setName(tagName);
     tagForm.getSlug();
     tagForm.clickSave();
+    cy.step('I create the tag <TAG>');
 
     tagsPage.visit();
+    cy.step('I navigate to tags page');
+
     tagsPage.click(tagName);
+    cy.step('I select the <TAG> tag');
 
     tagForm.clickDelete();
+    cy.step('I click on delete tag button');
+
+    cy.step('I cancel de deletion of the tag');
     tagForm.cancelDelete();
 
     tagsPage.visit();
+    cy.step('I navigate to tags page');
 
     tagsPage.tagLists().should('include.text', tagName);
+    cy.step('I should see the tag <TAG>');
   });
 });
