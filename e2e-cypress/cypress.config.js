bb7cb6f826203afa3f1177d6c765888e37518548
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-05-09 17:27:16 -0500

    feat(cypress): collect screenshots and build a JSON manifest

--- e2e-cypress/cypress.config.js
+++ e2e-cypress/cypress.config.js
@@ -7,15 +7,5 @@ module.exports = defineConfig({
     excludeSpecPattern: 'cypress/e2e/1_onboarding.cy.js',
     viewportWidth: 1200,
     viewportHeight: 900,
-    setupNodeEvents(on) {
-      on('after:screenshot', ({ path: imagePath }) => {
-        const name = imagePath.split('/').at(-1).split('.png')[0];
-        const [, , stepName] = name.split('---');
-        const step = {
-          name: stepName,
-          image: imagePath,
-        };
-      });
-    },
   },
 });
