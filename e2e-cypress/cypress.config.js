836804c3fd5a991b27d589705cf62824bee766c3
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-05-08 20:27:15 -0500

    feat(cypress): add step custom command

--- e2e-cypress/cypress.config.js
+++ e2e-cypress/cypress.config.js
@@ -5,5 +5,18 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: process.env.GHOST_BASE_URL || 'http://localhost:2368',
     excludeSpecPattern: 'cypress/e2e/1_onboarding.cy.js',
+    specPattern: ['cypress/e2e/delete-tag-1.cy.js', 'cypress/e2e/login.cy.js'], // TODO:
+    viewportWidth: 1200,
+    viewportHeight: 900,
+    setupNodeEvents(on) {
+      on('after:screenshot', ({ path: imagePath }) => {
+        const name = imagePath.split('/').at(-1).split('.png')[0];
+        const [, , stepName] = name.split('---');
+        const step = {
+          name: stepName,
+          image: imagePath,
+        };
+      });
+    },
   },
 });
