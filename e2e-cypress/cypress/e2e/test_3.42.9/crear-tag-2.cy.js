bd803c273ca9c01a33c532ff458b0f6226720283
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-05-10 17:51:05 -0500

    test(cypress): add steps to create-tag-2 ghost 3.42.9

--- e2e-cypress/cypress/e2e/test_3.42.9/crear-tag-2.cy.js
+++ e2e-cypress/cypress/e2e/test_3.42.9/crear-tag-2.cy.js
@@ -9,13 +9,6 @@ describe('T2 - Creación de tag con caracteres especiales', () => {
     const dashboardPage = new DashboardPage();
     const tagPages = new TagPage();
 
-    beforeEach(() => {
-        loginPage.visit();
-        loginPage.fillEmail(Cypress.env('email'));
-        loginPage.fillPassword(Cypress.env('password'));
-        loginPage.submit();
-    });
-
     it(`
     Given The login page
         And I sign-in with "<email>" and "<password>"
@@ -28,19 +21,40 @@ describe('T2 - Creación de tag con caracteres especiales', () => {
     Then I navigate to tags page
         And I should see the tag <TAG>
   `, () => {
+        loginPage.visit();
+        cy.step('The login page');
+
+        loginPage.fillEmail(Cypress.env('email'));
+        loginPage.fillPassword(Cypress.env('password'));
+        loginPage.submit();
+        cy.step('I sign-in with "<email>" and "<password>"');
+
+        dashboardPage.waitFor();
+        cy.step('I wait for the dashboard');
+
         dashboardPage.clickTagsLinkByHref();
+        cy.step('I navigate to tags page');
+
         tagPages.clickNewTagButtonByText();
+        cy.step('I select create New Tag');
+
         const specialString = generateSpecialString();
         tagPages.fillTagName(specialString);
+        cy.step('I create the tag name <TAG>');
+
         tagPages.fillTagDescription(faker.lorem.sentence());
+        cy.step('I create the tag description "<DESCRIPTION>"');
+
         tagPages.clickSaveButton();
+        cy.step('I click on the save Tag');
+
         tagPages.visit();
+        cy.step('I navigate to tags page');
+
         tagPages.verifyTag(specialString);
-
+        cy.step('I should see the tag <TAG>');
     });
-
 });
-
 
 function generateSpecialString() {
     const specialChars = "!@#$%^&*()";
