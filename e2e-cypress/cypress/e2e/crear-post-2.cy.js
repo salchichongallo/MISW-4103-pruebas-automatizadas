227d9490cfc9f347902eede8313f90cabb5ef29f
l.bogotab <l.bogotab@uniandes.edu.co>
2024-05-11 10:34:02 -0500

    [test]: add steps en crearpost2

--- e2e-cypress/cypress/e2e/crear-post-2.cy.js
+++ e2e-cypress/cypress/e2e/crear-post-2.cy.js
@@ -9,20 +9,29 @@ describe('Create a post', () => {
 
     beforeEach(() => {
         loginPage.visit();
+        cy.step('The login page');
         loginPage.fillEmail(Cypress.env('email'));
         loginPage.fillPassword(Cypress.env('password'));
+        cy.step('I sign-in with "<email>" and "<password>"');
         loginPage.submit();
-        dashboardPage.waitFor()
+        dashboardPage.waitFor();
+        cy.step('I wait for the dashboard');
     });
 
     it('T-10 - As a user I log in, enter the Posts section, create a new Members Only Post and validate your Members Only Post', () => {
         postsPage.visit();
+        cy.step('I navigate to post page');
         postsPage.newPost();
         postsPage.fillTitle('Members only');
+        cy.step('I type the title');
         postsPage.setVisibility('members');
+        cy.step('I set the visibility to members only');
         postsPage.publish();
+        cy.step('I publish the post');
         postsPage.backToEditor();
         postsPage.visitPublished();
+        cy.step('I navigate to posts published page');
         postsPage.verifyPost('Members only');
+        cy.step('the post is created and only visible to members');
     });
 });
