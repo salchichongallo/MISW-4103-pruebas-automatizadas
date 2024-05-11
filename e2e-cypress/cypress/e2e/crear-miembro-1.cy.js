02d6d1ae616a5f7efa2666943f4b1f70f9919cf6
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-05-11 11:20:06 -0500

    fix(cypress): wait for dashboard before start navigating

--- e2e-cypress/cypress/e2e/crear-miembro-1.cy.js
+++ e2e-cypress/cypress/e2e/crear-miembro-1.cy.js
@@ -1,27 +1,31 @@
+import { faker } from '@faker-js/faker';
+
 import { LoginPage } from './pages/LoginPage';
 import { MembersPage, MemberForm } from './pages/members-page';
+import { DashboardPage } from './pages/dashboard-page';
 
 describe('Member creation', () => {
   const loginPage = new LoginPage();
   const membersPage = new MembersPage();
   const memberForm = new MemberForm();
+  const dashboardPage = new DashboardPage();
 
   beforeEach(() => {
     loginPage.visit();
     loginPage.fillEmail(Cypress.env('email'));
     loginPage.fillPassword(Cypress.env('password'));
     loginPage.submit();
+    dashboardPage.waitFor();
   });
 
   it('Member is created properly', () => {
+    const email = faker.internet.email();
     membersPage.visit();
     membersPage.newMember();
     memberForm.setName('test member');
-    memberForm.setEmail('test@example.com');
+    memberForm.setEmail(email);
     memberForm.clickSave();
     membersPage.visit();
-    membersPage.verifyMember('test member', 'test@example.com');
+    membersPage.verifyMember('test member', email);
   });
 });
-
-
