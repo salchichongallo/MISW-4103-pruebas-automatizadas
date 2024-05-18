74ed0f4b4063ca4552bcddd1547a02bd8a31ecb8
Leiner Barrios <lj.barrios@uniandes.edu.co>
2024-05-17 23:19:51 -0500

    test: 💍 Se crean 10 escenarios apriori para creación de página

---
+++ e2e-cypress/cypress/e2e/pages_generate_data/page_apriori_5.cy.js
@@ -0,0 +1,30 @@
+import { LoginPage } from './pages/LoginPage';
+import { PagesPage } from './pages/pages-page';
+
+import pageDataMockaroo from './data-apriori-pages.json';
+
+describe('APRIORI: Create a page with title only', () => {
+  const loginPage = new LoginPage();
+  const pagesPage = new PagesPage();
+
+  beforeEach(() => {
+    loginPage.visit();
+    loginPage.fillEmail(Cypress.env('email'));
+    loginPage.fillPassword(Cypress.env('password'));
+    loginPage.submit();
+  });
+
+  it('As a user I log in, enter the Pages section, create a new Page with title only and verify its creation in the Pages list', () => {
+    pagesPage.visit();
+    cy.step('I navigate to pages page');
+    const pageForm = pagesPage.newPage();
+    pageForm.setTitle(pageDataMockaroo[4].title);
+    cy.step('I create the page title');
+    pageForm.publish();
+    cy.step('I publish the page');
+    pagesPage.visit();
+    cy.step('I navigate to pages page');
+    pagesPage.verifyPage('test page');
+    cy.step('I verify the page creation');
+  });
+});
---
+++ e2e-cypress/cypress/e2e/pages_generate_data/page_apriori_6.cy.js
@@ -0,0 +1,32 @@
+import { LoginPage } from './pages/LoginPage';
+import { PagesPage } from './pages/pages-page';
+
+import pageDataMockaroo from './data-apriori-pages.json';
+
+describe('APRIORI: Create a page with exist title', () => {
+  const loginPage = new LoginPage();
+  const pagesPage = new PagesPage();
+
+  beforeEach(() => {
+    loginPage.visit();
+    loginPage.fillEmail(Cypress.env('email'));
+    loginPage.fillPassword(Cypress.env('password'));
+    loginPage.submit();
+  });
+
+  it('As a user I log in, enter the Pages section, create a new Page with exist title and content and verify its creation in the Pages list', () => {
+    pagesPage.visit();
+    cy.step('I navigate to pages page');
+    const pageForm = pagesPage.newPage();
+    pageForm.setTitle(pageDataMockaroo[3].title);
+    cy.step('I create the page title');
+    pageForm.setContent(pageDataMockaroo[5].content);
+    cy.step('I create the page content');
+    pageForm.publish();
+    cy.step('I publish the page');
+    pagesPage.visit();
+    cy.step('I navigate to pages page');
+    pagesPage.verifyPage('test page');
+    cy.step('I verify the page creation');
+  });
+});
